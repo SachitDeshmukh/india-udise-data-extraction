6816372e1443516597261718f6f0777580f7dd4e
Sachit <admin@sachitdeshmukh.com>
2026-03-08 17:56:11 +0530

    Initial commit to code saving excel file

--- udise_api_calls/configuration.py
+++ udise_api_calls/configuration.py
@@ -4,3 +4,4 @@ UDISE_API_BASE_URL = "https://kys.udiseplus.gov.in/webapp/api/"
 API_RETRIES = 5
 URL_CHUNKS = 100
 PARALLEL_JOBS = 6
+EXCEL_FILE_BASE_TEXT = "UDISE_SCHOOL_DATA"
---
+++ udise_api_calls/save_excel_file.py
@@ -0,0 +1,8 @@
+# Importing all necessary libraries
+
+import pandas as pd
+import os
+from datetime import datetime
+
+import configuration
+import obtain_school_data_json
