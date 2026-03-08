c2aa83f8ed8408fc495f949b9630e097a233e40d
Sachit <admin@sachitdeshmukh.com>
2026-03-08 14:41:46 +0530

    Initial commit to start work on API workflow

---
+++ udise_api_calls/configuration.py
@@ -0,0 +1,4 @@
+# NOTING ALL CONFIG SETTING HERE
+
+UDISE_API_BASE_URL = "https://kys.udiseplus.gov.in/webapp/api/"
+PARALLEL_JOBS = 6
---
+++ udise_api_calls/get_all_api_url.py
@@ -0,0 +1,10 @@
+# Importing all necessary libraries
+
+import requests
+import json
+import logging
+
+# Set logging format and level
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
