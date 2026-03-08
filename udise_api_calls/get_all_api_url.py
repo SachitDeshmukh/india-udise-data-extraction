6ba8065718184e20881be39b423549b22e20ba1a
Sachit <admin@sachitdeshmukh.com>
2026-03-08 14:58:08 +0530

    Create modular functions to make api get calls

--- udise_api_calls/get_all_api_url.py
+++ udise_api_calls/get_all_api_url.py
@@ -3,8 +3,48 @@
 import requests
 import json
 import logging
+import configuration
 
 # Set logging format and level
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
+
+# Defining the GLOBALS
+
+BASE_URL: str = configuration.UDISE_API_BASE_URL
+
+
+def check_call_status(httpResponse: requests.Response):
+    request_message = httpResponse.json()
+    (
+        logging.info(f"Request status: {request_message["status"]}")
+        if httpResponse.status_code == 200
+        else logging.info(httpResponse)
+    )
+
+
+def make_get_call(target_url: str):
+    """
+    Makes GET API call.
+    """
+    api_session = requests.session()  # Initiate the API session
+
+    try:
+        call_response = api_session.get(url=target_url)
+        call_response.raise_for_status()
+    except requests.exceptions.ConnectionError as e:
+        raise e
+    except requests.exceptions.HTTPError as e:
+        raise e
+
+    check_call_status(call_response)
+
+
+def main():
+    trial_url = str(BASE_URL + "states?")
+    make_get_call(trial_url)
+
+
+if __name__ == "__main__":
+    main()
