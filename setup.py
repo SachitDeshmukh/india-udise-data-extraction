422ad50bdb9ff4be8b533a66870b65b2a8bf92e1
Sachit <admin@sachitdeshmukh.com>
2026-03-28 17:25:18 +0530

    Add SETUP to install modules as packages

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""
+THIS FILE ALLOWS USER TO INSTALL PACKAGES DEFINED IN THE REPO
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="0.1.0",
+    packages=find_packages(),
+)
