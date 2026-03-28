7a7f3767e7f8f5e948eda3ec6a460ef3eecc0f08
Sachit <admin@sachitdeshmukh.com>
2026-03-28 17:28:43 +0530

    Assign adequate name to parent package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ THIS FILE ALLOWS USER TO INSTALL PACKAGES DEFINED IN THE REPO
 from setuptools import setup, find_packages
 
 setup(
-    name="mypackage",
+    name="jpaludise",
     version="0.1.0",
     packages=find_packages(),
 )
